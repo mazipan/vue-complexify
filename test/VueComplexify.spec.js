d08c526c03807b8e55778fb3c21841a4725c2f90
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-09-18 10:12:58 +0700

    add gh link

--- test/VueComplexify.spec.js
+++ test/VueComplexify.spec.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 
-import { mount } from 'avoriaz'
-import VueComplexify from '../src/VueComplexify'
+// import { mount } from 'avoriaz'
+// import VueComplexify from '../src/VueComplexify'
 
 describe('VueComplexify.js', () => {
   
